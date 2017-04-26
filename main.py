6dea3ee34aa17bc586eb4568d3903f766037d7db
Ihor Polyakov <ihor.polyakov@gmail.com>
2017-04-26 16:31:49 +0700

    Additional logging

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import re
+import sys
+from datetime import datetime
 
 import requests
 
@@ -6,14 +8,19 @@ fake_variable = ''  # it's needed to set special order for imports below
 from settings import *
 from local_settings import *
 
+
+def eprint(*args, **kwargs):
+    print(datetime.now(), *args, file=sys.stderr, **kwargs)
+
+
 if not telegram_bot_key:
-    print('Empty API key (telegram_bot_key)')
+    eprint('Empty API key (telegram_bot_key)')
     exit(1)
 if not steam_profile:
-    print('Empty steam profile name (steam_profile)')
+    eprint('Empty steam profile name (steam_profile)')
     exit(2)
 if not telegram_chat_id:
-    print('Empty chat id (telegram_chat_id)')
+    eprint('Empty chat id (telegram_chat_id)')
     exit(3)
 
 telegram_api_url = 'https://api.telegram.org/bot%s/%s'
@@ -22,9 +29,9 @@ response = requests.get(url)
 content = response.content.decode()
 match = re.search('<div class="recentgame_quicklinks recentgame_recentplaytime">\s*<h2>(?P<phrase>.+)</h2>\s*</div>', content, re.MULTILINE | re.UNICODE)
 if not match:
-    print('Match not found')
+    eprint('Match not found')
     if DEBUG:
-        print(content)
+        eprint(content)
     exit(4)
 
 # i.e.: 16.0 ч. за последние 2 недели
@@ -39,7 +46,7 @@ if previous_phrase != phrase:
     file.write(phrase)
     file.close()
 else:
-    print('Same phrase as previous')
+    eprint('Same phrase as previous')
     exit(0)
 
 data = dict(
@@ -49,7 +56,7 @@ data = dict(
 url = telegram_api_url % (telegram_bot_key, 'sendMessage')
 response = requests.post(url, data)
 json_content = response.json()
-if not json_content:
-    print('Sending message error', json_content)
+if not json_content or not json_content['ok']:
+    eprint('Sending message error', json_content)
     exit(5)
-print('ok')
+print(datetime.now(), 'ok')
